4f583d41a43b5dbc97cd2673310c111c9b2460c5
Viren <viren@Gemini.local>
2013-09-11 09:46:30 +1000

    Rollback

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "dotfiles.sublime-project" \
-    --exclude "dotfiles.sublime-workspace" --exclude "install-deps.sh" -av --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
