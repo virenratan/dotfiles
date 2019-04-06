824c9b27879bb4127c69a2579e2e79eebc3d48b4
Viren Ratan <viren@viren.com.au>
2019-04-06 13:39:10 +1100

    🔧 tweaks to dock setup and update fish completions

--- dock-setup.sh
+++ dock-setup.sh
@@ -3,7 +3,9 @@ defaults write com.apple.dock persistent-apps -array
 
 # manually add app icons to dock in preferred order.
 dockutil --add  '/Applications/Google Chrome.app' --replacing 'Google Chrome'
-dockutil --add  '/Applications/Visual Studio Code.app' --replacing 'Visual Studio Code' --after 'Google Chrome'
+dockutil --add  '/Applications/Firefox.app' --replacing 'Firefox' --after 'Google Chrome'
+dockutil --add  '/Applications/Safari.app' --replacing 'Safari' --after 'Firefox'
+dockutil --add  '/Applications/Visual Studio Code.app' --replacing 'Visual Studio Code' --after 'Safari'
 dockutil --add  '/Applications/Tower.app' --replacing 'Tower' --after 'Visual Studio Code'
 dockutil --add  '/Applications/iTerm.app' --replacing 'iTerm' --after 'Tower'
 dockutil --add  '/Applications/Abstract.app' --replacing 'Abstract' --after 'iTerm'
--- fish/aliases.fish
+++ fish/aliases.fish
@@ -80,4 +80,4 @@ alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo 
 
 # Update installed Ruby gems, Homebrew, npm, and their installed packages
 alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor; npm-check -g -u"
-alias update_brew_npm_gem='brew_update; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update --no-document'
+alias update_brew_npm_gem='brew_update; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update --no-document; fish_update_completions'
