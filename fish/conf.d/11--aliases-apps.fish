d38bc0896c85fe6f53a00ab78001ce1c0c823c79
Viren Ratan <viren@viren.com.au>
2025-09-06 20:58:01 +1000

    ✨ better output for plex restart from log data

--- fish/conf.d/11--aliases-apps.fish
+++ fish/conf.d/11--aliases-apps.fish
@@ -7,12 +7,19 @@ abbr -a top 'vtop'
 abbr -a cask 'brew cask'
 abbr -a upgrade 'yarn global upgrade && brew upgrade && mas upgrade'
 
-# nas
-alias plex-restart "$HOME/Library/Mobile Documents/com~apple~CloudDocs/scripts/plex-restart.sh"
+# plex on synology nas.
+function plex-restart
+    ~/Library/Mobile\ Documents/com~apple~CloudDocs/scripts/plex-restart.sh
+    plex-status
+end
 function plex-status
     set plex_state (ssh viren@192.168.1.135 "sudo /usr/syno/bin/synopkg status PlexMediaServer | jq -r .status")
+    set now (date "+%Y-%m-%d %H:%M:%S")
+    set last_restart (ssh viren@192.168.1.135 "grep 'Plex Media Server v' '/volume1/PlexMediaServer/AppData/Plex Media Server/Logs/Plex Media Server.log' | tail -1 | cut -d' ' -f1-4")
+
     if test "$plex_state" = "running"
         echo "✅ Plex is running"
+        echo "   Last restarted at: $last_restart"
     else
         echo "❌ Plex is not running"
     end
