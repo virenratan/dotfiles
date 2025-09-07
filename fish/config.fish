271604b6d1d076701851e6403e3135b426bb4ff1
Viren Ratan <viren@viren.com.au>
2025-09-07 10:06:55 +1000

    🩹 formatting

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ function fish_greeting
 end
 set default_user $USER
 set default_machine (hostname -s)
-set -gx EDITOR code
+set -gx EDITOR nano
 test -e $HOME/.extra.fish; and source $HOME/.extra.fish
 
 # majority of configuration is under conf.d.
