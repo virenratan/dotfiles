325dfbbd69397c89cc996f45d6db11b18d613057
Viren Ratan <viren@viren.com.au>
2025-12-20 19:58:08 +1100

    ➕ added pocket-casts

--- dock-setup.sh
+++ dock-setup.sh
@@ -8,12 +8,11 @@ dockutil --add '/System/Applications/Finder.app' --replacing 'Finder'
 dockutil --add '/Applications/Google Chrome.app' --replacing 'Google Chrome' --after 'Finder'
 dockutil --add '/Applications/Visual Studio Code.app' --replacing 'Visual Studio Code' --after 'Google Chrome'
 dockutil --add '/System/Applications/Notes.app' --replacing 'Notes' --after 'Visual Studio Code'
-dockutil --add '/System/Applications/Music.app' --replacing 'Music' --after 'Notes'
-dockutil --add '/Applications/iTerm2.app' --replacing 'iTerm2' --after 'Music'
-dockutil --add '/Applications/Docker.app' --replacing 'Docker' --after 'iTerm2'
-dockutil --add '/Applications/Figma.app' --replacing 'Figma' --after 'Docker'
-dockutil --add '/Applications/Slack.app' --replacing 'Slack' --after 'Figma'
-dockutil --add '/System/Applications/iPhone Mirroring.app' --replacing 'iPhone Mirroring' --after 'Slack'
+dockutil --add '/Applications/Slack.app' --replacing 'Slack' --after 'Notes'
+dockutil --add '/System/Applications/Music.app' --replacing 'Music' --after 'Slack'
+dockutil --add '/Applications/Pocket Casts.app' --replacing 'Pocket Casts' --after 'Music'
+dockutil --add '/Applications/iTerm2.app' --replacing 'iTerm2' --after 'Pocket Casts'
+dockutil --add '/Applications/Figma.app' --replacing 'Figma' --after 'iTerm2'
 
 # restart dock to apply changes.
 killall Dock
